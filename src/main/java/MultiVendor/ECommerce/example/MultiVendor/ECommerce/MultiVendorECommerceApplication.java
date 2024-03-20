066e5bd4ab09e9e77717d934a54bc72891db49b1
ziadAshraf7 <ziadashraf454@icloud.com>
2024-03-20 21:11:36 +0200

    uplaoding project initial files

---
+++ src/main/java/MultiVendor/ECommerce/example/MultiVendor/ECommerce/MultiVendorECommerceApplication.java
@@ -0,0 +1,13 @@
+package MultiVendor.ECommerce.example.MultiVendor.ECommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiVendorECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiVendorECommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/MultiVendor/ECommerce/example/MultiVendor/ECommerce/MultiVendorECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package MultiVendor.ECommerce.example.MultiVendor.ECommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiVendorECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
